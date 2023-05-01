fb92b4070fb8a3e232dc0ec999bae0cc7c82bf49
Anton <An_freiheit@mail.ru>
2023-05-01 23:11:59 +0300

    scv test

--- src/test/java/RestSchemeTest.java
+++ src/test/java/RestSchemeTest.java
@@ -1,19 +1,30 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestSchemeTest {
 
-    @Test
-    public void test() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    /*
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    */
+    public void test(int expected, int income, int expenses, int threshold) {
         RestScheme service = new RestScheme();
 
-        int expected = 3;
-        int count = service.calculate(10_000, 3_000, 20_000);
+        //int expected = 3;
+        int count = service.calculate(income, expenses, threshold);
         //System.out.println("1. " + count + " раз(а) отдыхал за год");
 
         Assertions.assertEquals(expected, count);
     }
 
+    /*
     @Test
     public void test2() {
         RestScheme service = new RestScheme();
@@ -24,4 +35,5 @@ public class RestSchemeTest {
 
         Assertions.assertEquals(expected, count);
     }
+    */
 }
